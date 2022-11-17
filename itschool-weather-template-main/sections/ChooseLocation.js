111be909896c29f1b770cb953c519fc3187460ba
clarisacosma <cosma.clarisa@gmail.com>
2022-11-17 19:13:29 +0200

    functionalitate de schimbare oras

---
+++ itschool-weather-template-main/sections/ChooseLocation.js
@@ -0,0 +1,20 @@
+const bucharest = document.querySelector(".bucharest");
+const timisoara = document.querySelector(".timisoara");
+const oradea = document.querySelector(".oradea");
+
+function updateCity(city) {
+  const currentCity = document.getElementById("current-city");
+  currentCity.innerHTML = city;
+
+  displayCurrentWeather(city);
+}
+
+bucharest.addEventListener("click", function () {
+  updateCity("București");
+});
+timisoara.addEventListener("click", function () {
+  updateCity("Timișoara");
+});
+oradea.addEventListener("click", function () {
+  updateCity("Oradea");
+});
